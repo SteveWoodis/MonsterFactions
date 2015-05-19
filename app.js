41ee6beccb9a2a8a510e111a59b1a8f591db0542
Steve <swoodis2010@gmail.com>
2015-05-18 18:59:04 -0600

    I think I finally fixed the chat. At least no errors.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var app = angular.module('monsterApp', ['firebase', 'ngRoute']);
+var app = angular.module('monsterApp', ['firebase', 'ngRoute', "pubnub.angular.service"]);
 app.config(function($routeProvider){
     $routeProvider
+    .when('/join', {
+      templateUrl: 'views/join.html',
+      controller: 'JoinCtrl'
+    }).when('/chat', {
+      templateUrl: 'views/chat.html',
+      controller: 'ChatCtrl'
+    })
     .when('/landing', {
         templateUrl: '/landing.html',
         controller: 'landingCtrl'
@@ -9,17 +16,13 @@ app.config(function($routeProvider){
         templateUrl: '/login/login.html',
         controller: 'loginController'
     })
-    .when('/mChat', {
-        templateUrl: '/mChat/views/monsterChat-Index.html',
-        controller: 'chatCtrl'
-    })
     .when('/registration', {
         templateUrl: '/login/contractorRegistration.html',
         controller: 'conRegController'
     })
-    .when('/aboutUs', {
-        templateUrl: 'AboutUs.html',
-        controller: 'aboutUsCtrl'
+    .when('/Gallery', {
+        templateUrl: 'mGallery.html',
+        controller: 'mGalleryCtrl'
     })
     .when('/contact', {
      templateUrl: '/contact.html',
@@ -29,32 +32,63 @@ app.config(function($routeProvider){
         //     return gprojectService.getProjects();
         //     }
         // }
-    })
+    }).
+      when('/store', {
+        templateUrl: 'ShoppingCart/partials/store.htm',
+        controller: storeController 
+      }).
+      when('/products/:productSku', {
+        templateUrl: 'ShoppingCart/partials/product.htm',
+        controller: storeController
+      }).
+      when('/cart', {
+        templateUrl: 'SHoppingCart/partials/shoppingCart.htm',
+        controller: storeController
+      })
     .when('/monsterNation', {
      templateUrl: '/monsterNation.html',
         controller: 'monsterNationCtrl'
     })
-    .when('/join', {
-      templateUrl: 'views/join.html',
-      controller: 'JoinCtrl'
-    }).otherwise({
-      redirectTo: '/join'
-    });
-
-//    .when('/item/:itemId', {
-//    templateUrl: 'projects/submitbids.html',
-//    controller: 'submitbidsCtrl',
-//     resolve: {
-//            editBidAmount: function (gprojectService, $route) {
-//            return gprojectService.getProject($route.current.params.projectId);
-//            },
-//            submitBidRef: function (gprojectService, $route) {
-//            return gprojectService.updateProject($route.current.params.projectId);
-//            }
-//        }
-//    })
     .otherwise({
         redirectTo: '/landing'
     })
     });
+// create a data service that provides a store and a shopping cart that
+// will be shared by all views (instead of creating fresh ones for each view).
+app.factory("DataService", function () {
 
+    // create store
+    var myStore = new store();
+
+    // create shopping cart
+    var myCart = new shoppingCart("MonsterStore");
+
+    // enable PayPal checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with PayPal, you have to create a merchant account with 
+    // PayPal. You can do that here:
+    // https://www.paypal.com/webapps/mpp/merchant
+    myCart.addCheckoutParameters("PayPal", "monster-Factions@gmail.com");
+
+    // enable Google Wallet checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with Google Wallet, you have to create a merchant account with 
+    // Google. You can do that here:
+    // https://developers.google.com/commerce/wallet/digital/training/getting-started/merchant-setup
+    myCart.addCheckoutParameters("Google", "500640663394527",
+        {
+            ship_method_name_1: "UPS Next Day Air",
+            ship_method_price_1: "20.00",
+            ship_method_currency_1: "USD",
+            ship_method_name_2: "UPS Ground",
+            ship_method_price_2: "15.00",
+            ship_method_currency_2: "USD"
+        }
+    );
+
+    // return data object with store and cart
+    return {
+        store: myStore,
+        cart: myCart
+    };
+});
