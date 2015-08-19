ac4ef480253de4ed2f93a4c6c81a61ae83688af4
Steve <swoodis2010@gmail.com>
2015-08-18 20:36:59 -0600

    time stamp in chat room is now working

--- mChat/scripts/controllers/main.js
+++ mChat/scripts/controllers/main.js
@@ -85,18 +85,32 @@
     $scope.controlChannel = '__controlchannel';
     $scope.channels = [];
     /* Publish a chat message*/
-
+   
     $scope.publish = function() {
       console.log('publish', $scope);
       if (!$scope.selectedChannel) {
         return;
       }
+      Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+      }
+      Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+      }
+      var newDate = new Date();
+      var datetime = newDate.today() + " " + " @ " + " " + newDate.timeNow();
+      $scope.datetime = datetime;
+
       PubNub.ngPublish({
+        
         channel: $scope.selectedChannel,
+        datetime: $scope.datetime,
         message: {
           text: $scope.newMessage,
+          time: $scope.datetime,
           user: $scope.data.username
         }
+
       });
       return $scope.newMessage = '';
     };
