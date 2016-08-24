214337d2a429d6d8e38ef3dc136bad72ea86644d
Steve Woodis <swoodis2010@gmail.com>
2016-08-23 20:33:29 -0600

    added multiple images on store.js

--- src/js/store.js
+++ src/js/store.js
@@ -3,8 +3,11 @@
 //
 function store() {
     this.products = [
-        new product("were-t", "Werevolf", "Run as fast as you can!", 12, 90, 0, 2, 0, 1, 2),
-        
+        new product("were-t", "Werevolf", "Run as fast as you can!", 19.99, 90, 0, 2, 0, 1, 2),
+        new product("were-t", "Werevolf", "Run as fast as you can!", 19.99, 90, 0, 2, 0, 1, 2),
+        new product("were-t", "Werevolf", "Run as fast as you can!", 19.99, 90, 0, 2, 0, 1, 2),
+        new product("were-t", "Werevolf", "Run as fast as you can!", 19.99, 90, 0, 2, 0, 1, 2),
+        new product("were-t", "Werevolf", "Run as fast as you can!", 19.99, 90, 0, 2, 0, 1, 2),
     ];
     
 }
