2047b6ba616778ede3419618e78a0ab53148ba38
Steve Woodis <swoodis2010@gmail.com>
2016-08-25 21:45:14 -0600

    added dropdown but it is not looking cool

--- src/js/mainController.js
+++ src/js/mainController.js
@@ -1,5 +1,17 @@
 var app = angular.module('monsterApp');
 app.controller('mainController', function($scope){
-   console.log('made it to the mainController');
+   $scope.Factions = ['Vampire', 'Werewolf', 'Zombie', 'Suggest'];
+
+   $scope.dropboxitemselected = function () {
+      alert("drop box item selected");
+   }
+   $scope.selectedItem;
+   $scope.dropboxitemselected = function (item) {
+
+      $scope.selectedItem = item;
+      alert($scope.selectedItem);
+   }
+   
+   
 });
 
