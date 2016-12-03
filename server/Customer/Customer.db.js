f0a0d194bf705028b0178a68e2ef093969c2aa69
Jonathan Aaron <jon.r.aaron@gmail.com>
2016-12-03 10:50:17 -0700

    Adds url for local db called 'monster-factions'. Removes postinstall script for running bower. Adds run script for starting server using nodemon

--- server/Customer/Customer.db.js
+++ server/Customer/Customer.db.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/monster-factions');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
